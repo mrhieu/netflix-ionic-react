d31fbc83d1682afad0df8cdba70583a2d04df614
Pham Van Hieu <mr_hie@yahoo.com>
2020-05-07 23:00:45 +0800

    Updates

---
+++ assets/img/avatars.js
@@ -0,0 +1,52 @@
+export default const USERS = [
+  {
+    id: 1,
+    name: 'Black Panther',
+    profileUrl: 'assets/img/avatars/blackpanther.png',
+  },
+  {
+    id: 2,
+    name: 'Black Widow',
+    profileUrl: 'assets/img/avatars/blackwidow.png',
+  },
+  {
+    id: 3,
+    name: 'Captain',
+    profileUrl: 'assets/img/avatars/captain.png',
+  },
+  {
+    id: 4,
+    name: 'Hieu Pham',
+    profileUrl: 'assets/img/avatars/hieu.png',
+  },
+  {
+    id: 5,
+    name: 'Bruce Banner (Hulk)',
+    profileUrl: 'assets/img/avatars/hulk.png',
+  },
+  {
+    id: 6,
+    name: 'Thanos',
+    profileUrl: 'assets/img/avatars/thanos.png',
+  },
+  {
+    id: 7,
+    name: 'Iron Man',
+    profileUrl: 'assets/img/avatars/ironman.png',
+  },
+  {
+    id: 8,
+    name: 'Scarlet Witch',
+    profileUrl: 'assets/img/avatars/scarwitch.png',
+  },
+  {
+    id: 9,
+    name: 'Spiderman',
+    profileUrl: 'assets/img/avatars/spiderman.png',
+  },
+  {
+    id: 10,
+    name: 'Thor',
+    profileUrl: 'assets/img/avatars/thor.png',
+  },
+]
---
+++ static/js/4.440b2b02.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnetflix-ionic-react"]=this["webpackJsonpnetflix-ionic-react"]||[]).push([[4],{175:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(15),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.n)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=4.440b2b02.chunk.js.map
